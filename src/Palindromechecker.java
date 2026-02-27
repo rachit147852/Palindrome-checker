83272a66acee02f3d1e1048628f2a69b35f6b487
rachit147852 <so3291500@gmail.com>
2026-02-27 14:35:01 +0530

    UC2:Hardcoded palindrome

--- src/Palindromechecker.java
+++ src/Palindromechecker.java
@@ -1,30 +1,6 @@
 import java.util.Scanner;
 
-public class UC2Palindromechecker {
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        String a;
-
-        System.out.println("Enter string:");
-        a = in.nextLine();
-
-        boolean isPalindrome = true;
-
-        for(int i = 0; i < a.length()/2; i++){
-            if(a.charAt(i) != a.charAt(a.length() - 1 - i)){
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if(isPalindrome)
-            System.out.println("It is palindrome");
-        else
-            System.out.println("Not a palindrome");
-    }
-}import java.util.Scanner;
-
-public class UC2Palindromechecker {
+public class Palindromechecker {
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         String a;
