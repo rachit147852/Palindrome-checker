5b5f69bb8a0dfcb19b1c0ecfa55342b120e55791
rachit147852 <so3291500@gmail.com>
2026-02-27 14:45:18 +0530

    UC3:Reverse palindrome

--- src/Palindromechecker.java
+++ src/Palindromechecker.java
@@ -7,17 +7,13 @@ public class Palindromechecker {
 
         System.out.println("Enter string:");
         a = in.nextLine();
+         String b="";
 
-        boolean isPalindrome = true;
+        for(int i =a.length()-1;i>=0;i--){
+            b+=a.charAt(i);
+                }
 
-        for(int i = 0; i < a.length()/2; i++){
-            if(a.charAt(i) != a.charAt(a.length() - 1 - i)){
-                isPalindrome = false;
-                break;
-            }
-        }
-
-        if(isPalindrome)
+        if(b.equals(a))
             System.out.println("It is palindrome");
         else
             System.out.println("Not a palindrome");
