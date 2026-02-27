2afaf87ef72b71aef46178db78bebc0593eb9797
rachit147852 <so3291500@gmail.com>
2026-02-27 15:04:14 +0530

    UC5:STACK

--- src/Palindromechecker.java
+++ src/Palindromechecker.java
@@ -1,30 +1,41 @@
 import java.util.Scanner;
 import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
-public class UseCase5PalindromeCheckerApp {
-
+public class Palindromechecker
+{
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=== Palindrome Checker App (UC5 - Stack Based) ===");
+        System.out.println("=== Palindrome Checker App (UC6 - Queue + Stack Based) ===");
         System.out.print("Enter a string: ");
 
         String input = scanner.nextLine();
+
         Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
         for (int i = 0; i < input.length(); i++) {
-            stack.push(input.charAt(i));
+            char ch = input.charAt(i);
+            stack.push(ch);
+            queue.add(ch);
         }
+
         boolean isPalindrome = true;
-        for (int i = 0; i < input.length(); i++) {
 
-            char poppedChar = stack.pop();
+        while (!stack.isEmpty()) {
 
-            if (input.charAt(i) != poppedChar) {
+            char fromStack = stack.pop();
+            char fromQueue = queue.remove();
+
+            if (fromStack != fromQueue) {
                 isPalindrome = false;
                 break;
             }
         }
+
         if (isPalindrome) {
             System.out.println("Result: The given string is a Palindrome.");
         } else {
